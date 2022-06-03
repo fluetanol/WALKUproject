2f24f982c0a153f71c3fcad5814813584a6280ac
Won Seok Kim <76798309+dnjstjr245@users.noreply.github.com>
2022-06-03 17:59:18 +0900

    feat: 현 위치 불러오기, 현 날씨 상태 애니메이션, 현재 기온 표시 기능 추가

--- app/src/main/java/com/konkuk/walku/src/main/home/weather/WeatherFragment.kt
+++ app/src/main/java/com/konkuk/walku/src/main/home/weather/WeatherFragment.kt
@@ -3,6 +3,8 @@ package com.konkuk.walku.src.main.home.weather
 import android.Manifest
 import android.annotation.SuppressLint
 import android.graphics.Point
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.os.Looper
 import android.view.View
@@ -16,9 +18,13 @@ import com.konkuk.walku.R
 import com.konkuk.walku.config.BaseFragment
 import com.konkuk.walku.databinding.FragmentWeatherBinding
 import com.konkuk.walku.src.main.home.weather.model.GetWeatherResponse
+import com.konkuk.walku.src.main.home.weather.model.ModelWeather
 import com.konkuk.walku.util.OpenApiCommon
+import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 // TODO 코드가 지저분하므로 깔끔하게 리팩토링할 예정 (...)
 
@@ -27,6 +33,10 @@ class WeatherFragment :
     WeatherFragmentView {
 
     private var curPoint : Point? = null    // 현재 위치의 격자 좌표를 저장할 포인트 객체입니다.
+    
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -44,8 +54,6 @@ class WeatherFragment :
         requestLocation()
 
         showLoadingDialog(requireActivity())
-
-        Glide.with(this).load(R.drawable.sunny_anim).into(binding.fragmentWeatherCurrentWeatherStateAnimView)
 
     }
 
@@ -68,7 +76,11 @@ class WeatherFragment :
 
                             // 현재 위치의 위경도를 격자 좌표로 변환
                             curPoint = OpenApiCommon().dfsXyConv(location.latitude, location.longitude)
-                            
+
+                            // 전역 변수에 저장
+                            latitude = location.latitude
+                            longitude = location.longitude
+
                             // 기상청 레트로핏 서비스 호출
                             WeatherService(this@WeatherFragment).tryGetWeather(
                                 numOfRows = 300,
@@ -113,21 +125,77 @@ class WeatherFragment :
         return currentTime.format(formatterForBaseTimeMinutes)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onGetWeatherSuccess(response: GetWeatherResponse) {
         // 테스트입니다.
         dismissLoadingDialog()
         showCustomToast("날씨 api 연동 성공")
+        if(response.response.header.resultCode=="00") {
+            val it = response.response.body.items.item
+            // 현재 시각부터 1시간 뒤의 날씨 6개를 담을 배열
+            val weatherArr = arrayOf(ModelWeather(), ModelWeather(), ModelWeather(), ModelWeather(), ModelWeather(), ModelWeather())
+            var index = 0
+            val totalCount = response.response.body.totalCount - 1
+            for (i in 0..totalCount) {
+                index %= 6
+                when(it[i].category) {
+                    "PTY" -> weatherArr[index].rainType = it[i].fcstValue     // 강수 형태
+                    "REH" -> weatherArr[index].humidity = it[i].fcstValue     // 습도
+                    "SKY" -> weatherArr[index].sky = it[i].fcstValue          // 하늘 상태
+                    "T1H" -> weatherArr[index].temp = it[i].fcstValue         // 기온
+                }
+                index++
+            }
 
+            binding.apply {
+                // 현재 기온 설정
+                fragmentWeatherCurrentWeatherTemperatureTextView.text = "${weatherArr[0].temp}°C"
+                // 현재 하늘 상태 애니메이션 설정
+                Glide.with(this@WeatherFragment).load(getWeatherImage(weatherArr[0].sky)).into(fragmentWeatherCurrentWeatherStateAnimView)
+                // 현재 내 위치 주소 (한글 주소로 표시)
+                // TODO 00구 00동 까지만 나오도록 변경할 예정
+                fragmentWeatherAddressTextView.text = getAddress(latitude, longitude)
 
-
-
-
-
+            }
+        }
     }
 
     override fun onGetWeatherFailure(message: String) {
         dismissLoadingDialog()
         showCustomToast("오류 : $message")
+    }
+
+
+    private fun getWeatherImage(skyCode : String) : Int {
+        // 하늘 상태
+        return when(skyCode) {
+            "1" -> R.drawable.sunny_anim                // 맑음
+            "3" ->  R.drawable.medium_cloud_anim        // 구름 많음
+            "4" -> R.drawable.heavy_cloud_anim          // 흐림
+            else -> R.drawable.ic_launcher_foreground   // 오류
+        }
+    }
+
+    // 좌표 -> 주소 변환
+    private fun getAddress(lat: Double, lng: Double): String {
+        val geoCoder = Geocoder(requireContext(), Locale.KOREA)
+        val address: ArrayList<Address>
+        var addressResult = "주소를 가져 올 수 없습니다."
+        try {
+            //세번째 파라미터는 좌표에 대해 주소를 리턴 받는 갯수로
+            //한좌표에 대해 두개이상의 이름이 존재할수있기에 주소배열을 리턴받기 위해 최대갯수 설정
+            address = geoCoder.getFromLocation(lat, lng, 1) as ArrayList<Address>
+            if (address.size > 0) {
+                // 주소 받아오기
+                val currentLocationAddress = address[0].getAddressLine(0)
+                    .toString()
+                addressResult = currentLocationAddress
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return addressResult
     }
 
 }
