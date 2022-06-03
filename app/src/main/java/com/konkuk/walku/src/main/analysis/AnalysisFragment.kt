21c5ad6e34376e59fde3a60e2a3e21f261d6ba13
kkh <kkho9654@naver.com>
2022-06-03 10:21:01 +0900

    1

--- app/src/main/java/com/konkuk/walku/src/main/analysis/AnalysisFragment.kt
+++ app/src/main/java/com/konkuk/walku/src/main/analysis/AnalysisFragment.kt
@@ -2,12 +2,20 @@ package com.konkuk.walku.src.main.analysis
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.tabs.TabLayoutMediator
 import com.konkuk.walku.R
 import com.konkuk.walku.config.BaseFragment
 import com.konkuk.walku.databinding.FragmentAnalysisBinding
+import com.konkuk.walku.src.main.analysis.statistics.StatisticsFragment
+import com.konkuk.walku.src.main.home.HomeAdapter
+import com.konkuk.walku.src.main.home.map.MapFragment
+import com.konkuk.walku.src.main.home.weather.WeatherFragment
 
 class AnalysisFragment : BaseFragment<FragmentAnalysisBinding>(FragmentAnalysisBinding::bind, R.layout.fragment_analysis),
     AnalysisFragmentView {
+
+    private val fragmentList = listOf(StatisticsFragment())
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,4 +23,13 @@ class AnalysisFragment : BaseFragment<FragmentAnalysisBinding>(FragmentAnalysisB
 
     }
 
+    private fun setSwipeView() {
+        val adapter = HomeAdapter(requireActivity())
+        adapter.fragmentList = fragmentList
+        binding.fragmentHomeViewPager.adapter = adapter
+        val tabTitles = listOf("오늘의 날씨", "지도")
+        TabLayoutMediator(binding.fragmentHomeTapLayout, binding.fragmentHomeViewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
 }
