6519beabae72b329a6c6a16485ca2f670cd73e58
Won Seok Kim <76798309+dnjstjr245@users.noreply.github.com>
2022-06-10 11:47:55 +0900

    feat: 서울 주요공원현황 bottom sheet dialog 기능 추가

--- app/src/main/java/com/konkuk/walku/src/main/home/map/walkMap/model/ModelPark.kt
+++ app/src/main/java/com/konkuk/walku/src/main/home/map/walkMap/model/ModelPark.kt
@@ -1,6 +1,7 @@
 package com.konkuk.walku.src.main.home.map.walkMap.model
 
 data class ModelPark(
+    val parkTag: String = "",         // 공원태그
     val parkName: String = "",        // 공원명
     val parkDescription: String = "", // 공원개요
     val parkMainEquip: String = "",   // 주요시설
@@ -8,5 +9,9 @@ data class ModelPark(
     val parkZone: String = "",        // 공원지역 (ex : 중구)
     val parkAddress: String = "",     // 공원주소
     val parkLongitude: String = "",   // 공원위도
-    val parkLatitude: String = ""     // 공원경도
+    val parkLatitude: String = "",    // 공원경도
+    val parkArea: String = "",        // 공원면적
+    val parkOpenDate: String = "",    // 공원개원일
+    val parkNumber: String = "",      // 공원번호
+    val parkRoad: String = ""         // 공원 오는길
 )
---
+++ app/src/main/java/com/konkuk/walku/util/ParkBottomSheetDialog.kt
@@ -0,0 +1,58 @@
+package com.konkuk.walku.util
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.konkuk.walku.R
+import com.konkuk.walku.src.main.home.map.walkMap.model.ModelPark
+
+class ParkBottomSheetDialog(val itemId: Int, val parkArr: MutableList<ModelPark>, val itemClick: (Int) -> Unit) : BottomSheetDialogFragment() {
+
+    lateinit var parkImageView : AppCompatImageView
+    lateinit var titleTextView : TextView
+    lateinit var addressTextView : TextView
+    lateinit var areaAndOpenDateTextView : TextView
+    lateinit var tellTextView : TextView
+    lateinit var descriptionTextView : TextView
+    lateinit var equipTextView : TextView
+    lateinit var roadTextView : TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.park_bottom_sheet_dialog, container, false)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        parkImageView = view.findViewById(R.id.park_bottom_sheet_dialog_image_view)
+        titleTextView = view.findViewById(R.id.park_bottom_sheet_dialog_title_text_view)
+        addressTextView = view.findViewById(R.id.park_bottom_sheet_dialog_address_text_view)
+        areaAndOpenDateTextView = view.findViewById(R.id.park_bottom_sheet_dialog_area_and_open_date_text_view)
+        tellTextView = view.findViewById(R.id.park_bottom_sheet_dialog_tell_text_view)
+        descriptionTextView = view.findViewById(R.id.park_bottom_sheet_dialog_description_content_text_view)
+        equipTextView = view.findViewById(R.id.park_bottom_sheet_dialog_equip_content_text_view)
+        roadTextView = view.findViewById(R.id.park_bottom_sheet_dialog_road_content_text_view)
+
+        Glide.with(view).load(parkArr[itemId-1].parkImage).into(parkImageView)
+        titleTextView.text = parkArr[itemId-1].parkName
+        addressTextView.text = parkArr[itemId-1].parkAddress
+        areaAndOpenDateTextView.text = "${parkArr[itemId-1].parkArea}  ·  ${parkArr[itemId-1].parkOpenDate} 개원"
+        tellTextView.text = parkArr[itemId-1].parkNumber
+        descriptionTextView.text = parkArr[itemId-1].parkDescription
+        equipTextView.text = parkArr[itemId-1].parkMainEquip
+        roadTextView.text = parkArr[itemId-1].parkRoad
+    }
+    override fun getTheme(): Int {
+        return R.style.AppBottomSheetDialogTheme
+    }
+}
